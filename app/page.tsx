ecfb199918257848cf18a1f9b1ea53d230cb1855
root <root@uran.home>
2025-02-25 23:29:17 +0100

    some changes due to eslint strict rules

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,12 @@
 import { useState, useEffect } from 'react'
 import clsx from 'clsx';
 
+type Task = {
+  id: number,
+  text: string,
+  completed: boolean
+}
+
 export default function Home() {
   const [tasks, setTasks] = useState(() => {
     // read data from localstorage
@@ -38,8 +44,8 @@ export default function Home() {
     }
   }
 
-  const handleCheckboxChange = (id: Date) => {
-    setTasks(tasks.map((task: { id: Date; completed: any; }) => 
+  const handleCheckboxChange = (id: number) => {
+    setTasks(tasks.map((task: { id: number; completed: boolean; }) => 
       task.id === id ? { ...task, completed: !task.completed } : task
     ));
   };
@@ -62,10 +68,12 @@ export default function Home() {
                   <th className='w-64'>Task</th>
                 </tr>
               </thead>
-              <tbody>
-                {tasks.map((task: any) => (
+              <tbody> 
+                {
+                
+                tasks.map((task: Task) => (
                   <tr key={task.id}>
-                    <td><label className={clsx("flex items-center cursor-pointer transition-200", { "hidden": task.completed, "block": !task.completed })}><input id={task.id} type="checkbox" className="hidden peer" onChange={() => {handleCheckboxChange(task.id)}} /><span className="w-5 h-5 border-2 border-gray-300 rounded mr-2 flex items-center justify-center peer-checked:bg-green-300 peer-checked:border-green-500"><svg className="hidden w-3 h-3 text-white peer-checked:block" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l2 2 5-5-1.5-1.5L9 10.5 8 9.5z" /></svg></span></label><label className={clsx("flex items-center cursor-pointer transition-200", { "hidden": !task.completed, "block": task.completed })}><input checked id={task.id} type="checkbox" className="hidden peer" onChange={() => {handleCheckboxChange(task.id)}} /><span className="w-5 h-5 border-2 border-gray-300 rounded mr-2 flex items-center justify-center peer-checked:bg-green-300 peer-checked:border-green-500"><svg className="hidden w-3 h-3 text-white peer-checked:block" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l2 2 5-5-1.5-1.5L9 10.5 8 9.5z" /></svg></span></label></td>
+                    <td><label className={clsx("flex items-center cursor-pointer transition-200", { "hidden": task.completed, "block": !task.completed })}><input id={String(task.id)} type="checkbox" className="hidden peer" onChange={() => {handleCheckboxChange(task.id)}} /><span className="w-5 h-5 border-2 border-gray-300 rounded mr-2 flex items-center justify-center peer-checked:bg-green-300 peer-checked:border-green-500"><svg className="hidden w-3 h-3 text-white peer-checked:block" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l2 2 5-5-1.5-1.5L9 10.5 8 9.5z" /></svg></span></label><label className={clsx("flex items-center cursor-pointer transition-200", { "hidden": !task.completed, "block": task.completed })}><input checked id={String(task.id)} type="checkbox" className="hidden peer" onChange={() => {handleCheckboxChange(task.id)}} /><span className="w-5 h-5 border-2 border-gray-300 rounded mr-2 flex items-center justify-center peer-checked:bg-green-300 peer-checked:border-green-500"><svg className="hidden w-3 h-3 text-white peer-checked:block" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l2 2 5-5-1.5-1.5L9 10.5 8 9.5z" /></svg></span></label></td>
                     <td><label className={clsx("transition-all duration-200 break-words", { "line-through text-grey-500": task.completed })} htmlFor={String(task.id)}>{task.text}</label></td>
                   </tr>
                 ))}
